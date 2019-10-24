b28eee24b6dc8447bbce6354e336817dfaa05697
Amilson Gaspar <agjdesenvolvimento@gmail.com>
2019-10-24 16:30:29 -0300

    Criado autenticaçao.
    Criado migrations de user e students, model user e controller de session
    com Jwt.

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  apiKey: '760e24220b354fbdd1c09bc889a83061',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,10 @@
 import { Router } from 'express';
-
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ message: 'Bem vindo ao Gympoint!' }));
+
+routes.post('/session', SessionController.store);
+
+export default routes;
